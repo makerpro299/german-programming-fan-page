722a51cd81830262507b34f6ba7d9dd3d56fe6a2
Max <max.heise18@gmail.com>
2021-06-04 16:59:03 +0200

    german-programming-fanpage
    Its a german Programming fan page with Tutorials, an Chatroom, and a Minigame. Check it out and enjoy.

---
+++ script.js
@@ -0,0 +1,34 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter=0;
+function jump(){
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500);
+}
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<10 && blockLeft>-10 && characterTop>=130){
+        block.style.animation = "none";
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    }
+var score = 0;
+var scoreSpan = localStorage.getItem("scoreSpan");
+
+if(scoreSpan !== null){
+    if (score > scoreSpan) {
+        localStorage.setItem("scoreSpan", score);      
+    }
+}
+else{
+    localStorage.setItem("scoreSpan", score);
+}
+}, 10);
